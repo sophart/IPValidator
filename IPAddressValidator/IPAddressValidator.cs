7496ac93fd53a352f1dc28b2812eff75f8c58153
Phun Sophart <phunsophart@gmail.com>
2023-01-04 22:31:03 +0700

    Update Comment to Make Code More Readable

--- IPAddressValidator/IPAddressValidator.cs
+++ IPAddressValidator/IPAddressValidator.cs
@@ -4,14 +4,17 @@ public class IPAddressValidator
 {
     public static bool Validate(string ipAddress)
     {
+        // ip address that has less or more than 4 octets
+        // consider invalid
         if (!ipAddress.HasFourOctets(out string[] octets)) return false;
 
+        // if there are no invalid octets in the ip address
+        // the ip consider valid
         return !octets.Any(
-            octet => !octet.HasValidNumberOfDigits()
-                     || !octet.HasNoLeadingZero()
-                     || !octet.IsParsableToDecimal(out int octetInDecimalFormat)
-                     || !octet.IsInValidRange(octetInDecimalFormat)
+            octet => !octet.HasValidNumberOfDigits() // one or more octets has 0 or more than 3 digits
+                     || !octet.HasNoLeadingZero() // one or more octets has leading zero
+                     || !octet.IsParsableToDecimal(out int octetInDecimalFormat) // one or more octets are not parsable to decimal
+                     || !octet.IsInValidRange(octetInDecimalFormat) // one or more octets is less than 0 or larger than 255
                      );
-        
     }
 }
--- IPAddressValidator/OctetValidatorExtensions.cs
+++ IPAddressValidator/OctetValidatorExtensions.cs
@@ -4,32 +4,30 @@ public static class OctetValidatorExtensions
 {
     public static bool HasValidNumberOfDigits(this string octet)
     {
-        // each octet contains only 1-3 digits
+        // octet must have only 1-3 digits
         return octet.Length <= 3 && octet.Length >= 1;
     }
 
     public static bool HasNoLeadingZero(this string octet)
     {
         // if octet has only 1 digit
-        // even if it is zero, it is valid
+        // zero should consider valid
         if (octet.Length == 1) return true;
         
         // if octet has more than 1 digit and 
-        // the first digit is 0, it is invalid
+        // the first digit is not 0, it is valid
         return !octet[0].ToString().Equals("0");
     }
 
     public static bool IsParsableToDecimal(this string octet, out int octetInDecimalFormat)
     {
-        // each octet should be parsable to decimal (base 10)
-        // if the octet is not parsable, it is invalid
+        // octet should be parsable to decimal (base 10)
         return int.TryParse(octet, out octetInDecimalFormat);
     }
 
     public static bool IsInValidRange(this string octet, int octetInDecimalFormat)
     {
-        // each octet in decimal (base 10) must be in range of 0 to 255
-        // if it is out of that range, it is invalid
+        // octet in decimal (base 10) must be in range from 0 to 255
         return octetInDecimalFormat >= 0 && octetInDecimalFormat <= 255;
     }
 }
