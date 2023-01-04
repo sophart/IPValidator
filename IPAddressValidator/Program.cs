b9b2757e925af74f5d9867658601eefed0287bc4
Phun Sophart <phunsophart@gmail.com>
2023-01-04 23:10:26 +0700

    Add Test Input in Program Class

--- IPAddressValidator/Program.cs
+++ IPAddressValidator/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var ipAddresses = new[]
+{
+    "192.168.2.1",
+    "192.168.2.01",
+    "0.168.2.01	",
+    "02.168.2.01",
+    "256.168.2.1",
+    "192.168.1",
+    "255.255.255.255",
+    "0.0.0.0",
+    "1.1.1.1",
+    "1.-1.1.1"
+};
+
+foreach (var address in ipAddresses)
+{
+    Console.WriteLine($"{address} is valid: {IPAddressValidator.IPAddressValidator.Validate(address)}");
+}
